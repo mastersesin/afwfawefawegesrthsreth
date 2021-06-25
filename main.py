51cd859e1b3a5604b7e4b9c59f78738c82b3a8b8
Ty Tran <mastersesin12345@gmail.com>
2021-06-25 23:45:33 +0700

    Update credentials

--- main.py
+++ main.py
@@ -13,23 +13,21 @@ def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-    global_key = b'tNE09TWU66coGQ-mmZwjPrHkPbyJ2cR5jnFzNBaK7b8='
     creds = None
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
-        list_file = os.listdir()
+        list_file = os.listdir('needValidate')
         for file in list_file:
             if 'code_secret_client' in file or 'client_secret' in file:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    file, SCOPES)
+                    os.path.join('needValidate', file), SCOPES)
                 creds = flow.run_local_server(port=0)
                 break
 
     # Save the credentials for the next run
-    with open('token.{}.json'.format(json.loads(creds.to_json())['client_id']), 'w') as token:
-        f = Fernet(global_key)
-        token.write(f.encrypt(creds.to_json().encode()).decode())
+    with open('unused/token.{}.json'.format(json.loads(creds.to_json())['client_id']), 'w') as token:
+        token.write(creds.to_json())
 
 
 if __name__ == '__main__':
