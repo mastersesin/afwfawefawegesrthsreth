3f1216b0c05f67b43721dfd9076becda653fea38
Ty Tran <ty@autonomous.nyc>
2021-07-02 01:44:03 +0700

    Final commit

--- script.py
+++ script.py
@@ -5,24 +5,27 @@ import threading
 import paramiko
 
 SCRIPT_CONTENT = """
+sudo pkill python3
+sudo pkill chia_plot
 rm -rf run.sh
 sudo apt-get update
 sudo mkfs -t xfs /dev/nvme2n1
 sudo mkdir -p /tmp1
 sudo mount /dev/nvme2n1 /tmp1
-sudo mkdir /tmp1/tmp1
 sudo mkdir /mnt/ramdisk1
 sudo rm -rf /mnt/ramdisk1/*
 sudo apt install -y libsodium-dev cmake g++ git
 sudo mount -t tmpfs -o size=110G tmpfs /mnt/ramdisk1/
-sudo chown -R ubuntu.ubuntu /tmp1
 cd /tmp1
+sudo rm -rf *
+sudo mkdir /tmp1/tmp1
 git clone https://github.com/mastersesin/afwfawefawegesrthsreth.git
 cd afwfawefawegesrthsreth
 sudo apt-get install -y python3-pip
 pip3 install -r requirements.txt
 mkdir uploading
 git checkout new
+sudo chown -R ubuntu.ubuntu /tmp1
 """
 PEM_PATH = 'pem'
 run_file = open('sshfile', 'r')
@@ -37,12 +40,12 @@ def worker(paramiko_ssh_key, paramiko_connect_ip):
     ssh.exec_command("chmod 777 run.sh")
     (ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command('./run.sh')
     for content in ssh_stdout.readlines():
-        print(content.rstrip())
+        content.rstrip()
     ssh.exec_command(
-        'tmux new-session -d -s "myTempSession1" /tmp1/afwfawefawegesrthsreth/chia_plot -t /tmp1/tmp1/ -d /tmp1/ -2 /mnt/ramdisk1/ -n -1 -r 70 -p a59e33b8108631810978bb60e9150ff9b4f38ad2e76776d8491acb7e7dacb906f9fff90ee6dcff9ca56d54ea20702f92 -f ad1b159147734d8a817e776e02ef0ced0ff61db9d6695c24bf7147a6622082c9ab59620697e9c434accdf3ab76bd1393')
+        'tmux new-session -d -s "myTempSession1" /tmp1/afwfawefawegesrthsreth/chia_plot -t /tmp1/tmp1/ -d /tmp1/ -2 /mnt/ramdisk1/ -n -1 -r 32 -p a94a9f827a062a24d8f8c2201f9113fd8428da53deded15d501d8c94ed59e7d700b44bdc7e0e42a1501426fccca005b6 -f ac99a1d74615b16d12189e2b82a51e0640ca1aa38c55a5841f78c58ac448972555585c8295c181a0eaf6a6d9bf5f5d2d')
     ssh.exec_command(
         'tmux new-session -d -s "myTempSession2" python3 /tmp1/afwfawefawegesrthsreth/worker.py')
-    print('done 1')
+    print('done {}'.format(paramiko_connect_ip))
 
 
 for line in run_file:
