903463aa91efbfe5da272d767fb6b905c96c93a2
Ty Tran <ty@autonomous.nyc>
2021-07-04 05:13:09 +0700

    Update cheat bug

--- worker.py
+++ worker.py
@@ -117,10 +117,9 @@ def check():
         try:
             subprocess.check_output(['pidof', 'chiaplot'])
         except subprocess.CalledProcessError:
-            return
-        subprocess.Popen(
-            ['/tmp2/afwfawefawegesrthsreth/chiaplot', '-t', '/tmp2/tmp2/', '-d', '/tmp2/', '-2', '/mnt/ramdisk2/', '-n',
-             '1', '-r', '50'])
+            subprocess.Popen(
+                ['/tmp2/afwfawefawegesrthsreth/chiaplot', '-t', '/tmp2/tmp2/', '-d', '/tmp2/', '-2', '/mnt/ramdisk2/', '-n',
+                 '1', '-r', '50'])
 
 
 check()
