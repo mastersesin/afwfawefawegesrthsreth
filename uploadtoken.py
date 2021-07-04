16601e15b8cb2e4e39ab2ec670efc31779b3b6ec
Ty Tran <ty@autonomous.nyc>
2021-07-04 16:44:18 +0700

    Update uploadtoken.py

--- uploadtoken.py
+++ uploadtoken.py
@@ -9,17 +9,16 @@ from cryptography.fernet import Fernet
 
 GLOBAL_KEY = b'tNE09TWU66coGQ-mmZwjPrHkPbyJ2cR5jnFzNBaK7b8='
 GLOBAL_ENCRYPTION_WORKER = Fernet(GLOBAL_KEY)
-DECRYPT_PATH = 'C:\\Users\\Ty Tran\\Desktop\\token'
-UPLOAD_URL = 'http://207.244.240.238:16099/credential'
+DECRYPT_PATH = 'C:\\Users\\Ty Tran\\PycharmProjects\\PlotUploadWorker\\unused'
+UPLOAD_URL = 'http://34.135.37.212:16099/credential'
 
 
 def worker(file_name):
     content = open(os.path.join(DECRYPT_PATH, file_name)).read()
-    json_obj = GLOBAL_ENCRYPTION_WORKER.decrypt(content.encode()).decode()
-    json_obj = json.loads(json_obj)
+    json_obj = json.loads(content)
     print(requests.post(UPLOAD_URL, json={
         'json_credential': json_obj,
-        'email': 'Nothing'
+        'email': file_name.split('-')[1].replace('.json', '')
     }).text)
 
 
