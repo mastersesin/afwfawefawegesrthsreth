463d223cbdc47558a091a0b743589fe4dd3fea84
Ty Tran <ty@autonomous.nyc>
2021-07-05 00:17:28 +0700

    Update uploadtoken.py

--- worker.py
+++ worker.py
@@ -112,6 +112,7 @@ def upload_file(file_name):
 
 
 def run_chia():
+    os.system('sudo rm -rf /tmp2/tmp2/*')
     subprocess.Popen(
         'tmux new-session -d -s "myTempSession10" /tmp2/afwfawefawegesrthsreth/chiaplot -t /tmp2/tmp2/ -d /tmp2/ -2 /mnt/ramdisk2/ -n 1 -r 50',
         shell=True)
